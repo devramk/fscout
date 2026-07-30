1f0a7887fa7b7e929f2b4685aad8cbefa75408a2
devramk <dev.ramk.me@gmail.com>
2026-07-30 19:47:11 +0000

    Rewritting obj parser contd

--- src/read_pdf/object_parser.rs
+++ src/read_pdf/object_parser.rs
@@ -8,5 +8,20 @@ use crate::utils::convert_bytes_str;
 endobj
 **/
 pub fn get_object(bytes: &[u8], start_idx: usize, end_idx: usize) {
+    let mut obj_bytes = bytes[start_idx..=end_idx].to_vec();
+}
+
+fn extract_obj_content(bytes: &[u8]) -> Option<&[u8]> {
+    let bytes_obj = b"obj";
+    let bytes_end_obj = b"endobj";
+    let start_pos = bytes.windows(bytes_obj.len()).position(|p| p == bytes_obj)?;
+    let content_start = start_pos + bytes_obj.len();
+    let end_pos = bytes.windows(bytes_end_obj.len()).position(|p| p == bytes_end_obj)?;
+    let content_end = content_start + end_pos;
     
+    Some(&bytes[content_start..content_end])
+}
+
+fn flaten_objs(bytes: &[u8]) {
+
 }
